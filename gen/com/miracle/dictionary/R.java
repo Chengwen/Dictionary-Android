5a4f132003713e20d8063d9eed151f63fafaf296
Chengwen <chengwen.sui@gmail.com>
2014-07-13 13:05:23 -0400

    UI

--- gen/com/miracle/dictionary/R.java
+++ gen/com/miracle/dictionary/R.java
@@ -8,6 +8,9 @@
 package com.miracle.dictionary;
 
 public final class R {
+    public static final class array {
+        public static final int country_arrays=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -25,15 +28,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int container=0x7f080000;
+        public static final int action_settings=0x7f090007;
+        public static final int button1=0x7f090003;
+        public static final int container=0x7f090000;
+        public static final int editText1=0x7f090006;
+        public static final int language1=0x7f090002;
+        public static final int language2=0x7f090004;
+        public static final int linearLayout1=0x7f090001;
+        public static final int linearLayout2=0x7f090005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fragment_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
@@ -62,10 +71,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
