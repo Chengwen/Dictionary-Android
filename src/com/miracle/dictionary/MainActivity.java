c6a205c17a6b8b860f91aaec037da5bf9756a6e2
Chengwen <chengwen.sui@gmail.com>
2014-07-16 18:18:04 -0400

    test

--- src/com/miracle/dictionary/MainActivity.java
+++ src/com/miracle/dictionary/MainActivity.java
@@ -18,19 +18,35 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 import android.os.Build;
 
 public class MainActivity extends Activity {
-
+	
+	 private AutoCompleteTextView autotext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+      //获取布局文件中的两个控件对象
+        autotext=(AutoCompleteTextView)findViewById(R.id.autotext);
+     
+        //设置数据源
+        String[] autoStrings=new String[]{"New York","Tokyo","beijing","london","Seoul Special","Los Angeles"};
+        //设置ArrayAdapter，并且设定以单行下拉列表风格展示（第二个参数设定）。
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity.this, 
+android.R.layout.simple_dropdown_item_1line, autoStrings);
+        //设置AutoCompleteTextView的Adapter
+        autotext.setAdapter(adapter);
+       
 
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
