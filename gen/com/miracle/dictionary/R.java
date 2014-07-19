30af6d42903edb84d3de4397e7106b6bfe3eb566
Chengwen <chengwen.sui@gmail.com>
2014-07-19 09:46:55 -0400

    Change some UI

--- gen/com/miracle/dictionary/R.java
+++ gen/com/miracle/dictionary/R.java
@@ -8,9 +8,6 @@
 package com.miracle.dictionary;
 
 public final class R {
-    public static final class array {
-        public static final int country_arrays=0x7f070000;
-    }
     public static final class attr {
     }
     public static final class dimen {
@@ -28,26 +25,28 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f0a0001;
-        public static final int action_settings=0x7f0a0005;
-        public static final int autoCompleteTextView1=0x7f0a0004;
-        public static final int container=0x7f0a0000;
-        public static final int list=0x7f0a0003;
-        public static final int searchView1=0x7f0a0002;
+        public static final int LinearLayout1=0x7f090000;
+        public static final int action_settings=0x7f090006;
+        public static final int autoCompleteTextView1=0x7f090005;
+        public static final int autotext=0x7f090001;
+        public static final int cancel=0x7f090002;
+        public static final int scrollView1=0x7f090003;
+        public static final int searchResult=0x7f090004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
-        public static final int rounded_editview=0x7f030002;
-        public static final int search_result=0x7f030003;
+        public static final int rounded_editview=0x7f030001;
+        public static final int search_result=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f090000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060002;
+        public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060001;
+        public static final int cancel=0x7f060004;
+        public static final int search_dictionary=0x7f060002;
+        public static final int word_not_find=0x7f060003;
     }
     public static final class style {
         /** 
@@ -71,11 +70,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080001;
+        public static final int AppTheme=0x7f070001;
     }
     public static final class xml {
         public static final int searchable=0x7f040000;
